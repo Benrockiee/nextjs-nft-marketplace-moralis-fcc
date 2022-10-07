ce26a2c949d5a553fe3e6571e8493d59a5657c71
Benjamin izuchukwu umeike <benrockiee@gmail.com>
2022-10-07 03:02:26 -0700

    Comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+//Everything runs through this page>> We have our home page which is the index.js
+// So we make a sell page folder >> sell-nft.js but we first start building our
+//home page which is the index.js
 import "../styles/globals.css"
-import { MoralisProvider } from "react-moralis"
-import Header from "../components/Header"
+import { MoralisProvider } from "react-moralis" //After importing this, we wrap up our component thing in our
+import Header from "../components/Header" // moralis provider and go back to our header and say
+//  export default function Header() BACK TO HEADER NOW >>>>
 import Head from "next/head"
 import { NotificationProvider } from "web3uikit"
-
+//we pull in our dotenv files in here >>>.
 const APP_ID = process.env.NEXT_PUBLIC_APP_ID
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 
@@ -15,6 +19,7 @@ function MyApp({ Component, pageProps }) {
                 <meta name="description" content="NFT Marketplace" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+
             <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
                 <NotificationProvider>
                     <Header />
@@ -26,3 +31,5 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+//<MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}> Here we put our App ID and the url server from Moralis
