ca9b0ebae9946d429cf571dd955336a3e23c2e4b
Benjamin izuchukwu umeike <benrockiee@gmail.com>
2022-10-07 03:04:07 -0700

    Comments

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+//This allows us to use and fetch queries from our data base in a react context
 import { useMoralisQuery, useMoralis } from "react-moralis"
 import NFTBox from "../components/NFTBox"
-
+//First thing we always need is to create a connect button so we make a component folder and create new file
+//>> Header.js so we move over to that file now
 export default function Home() {
     const { isWeb3Enabled } = useMoralis()
+    //To get all of our active items from our Database and check if this query is still fetching, we say:
     const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
         // TableName
         // Function for the query
@@ -44,3 +47,20 @@ export default function Home() {
         </div>
     )
 }
+/* We did this to check if we are fetching those NFTS
+      fetchingListedNfts ? (
+        <div>Loading...</div>
+        ) : ( 
+            listedNfts.map((nft) =>  //THIS means that we are basically going to loop through each NFT
+             console.log(nft.attributes) //Inside these attributes are the attributes we want like price, etc..
+                            const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                                nft.attributes
+                            return ( //We pull them out by returning it
+                                <NFTBox  //we add our NFT Box component an pass in all the parameters it takes
+                                    price={price}
+                                    nftAddress={nftAddress}
+                                    tokenId={tokenId}
+                                    marketplaceAddress={marketplaceAddress}
+                                    seller={seller}
+                              key={`${nftAddress}${tokenId}`} //nftAddress combined with the tokenId can be the key
+                                    */
